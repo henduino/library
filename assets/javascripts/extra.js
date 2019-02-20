5be4970dc655aaec139b4370384d07d968d1b051
hendriono <hendriono@gmail.com>
2019-02-21 06:40:10 +0700

    Deployed 488094b with MkDocs version: 1.0.4

--- assets/javascripts/extra.js
+++ assets/javascripts/extra.js
@@ -1,3 +1,14 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+  var progressBar = document.querySelector('.progress-bar');
+  function onProgress(x, y) {
+    console.log(x, y)
+    progressBar.style.width = y * 100 + '%';
+  }
+  self.progressObserver = new ScrollProgress(onProgress);
+  }, 100)
+});
+
 window.MathJax = {
   tex2jax: {
     inlineMath: [ ["\\(","\\)"] ],
